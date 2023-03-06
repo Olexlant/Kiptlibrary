4993a833d677fe6fae0df5bf4c1104a606e212c2
Olexlant <zgn5bx3q7>
2023-03-06 15:35:40 +0200

    messages encoding update

--- src/main/java/com/Kipfk/Library/security/config/WebConfig.java
+++ src/main/java/com/Kipfk/Library/security/config/WebConfig.java
@@ -1,7 +1,9 @@
 package com.Kipfk.Library.security.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
@@ -17,6 +19,15 @@ public class WebConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
 
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource resource =
+                new ResourceBundleMessageSource();
+        resource.setBasename("messages");
+        resource.setDefaultEncoding("UTF-8");
+        return resource;
     }
     @Bean
     public LocaleResolver localeResolver() {
