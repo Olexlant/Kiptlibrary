90282f25add98d567ae1b31acd27a52c23bdc4a3
Pavlo Berlet <pasha.berlet@gmail.com>
2023-03-07 20:21:20 +0200

    add blok news

---
+++ src/main/resources/static/js/slider-news.js
@@ -0,0 +1,8 @@
+
+    document.addEventListener('DOMContentLoaded', function () {
+      // инициализация слайдера
+      var slider = new SimpleAdaptiveSlider('.slider', {
+        autoplay: true,
+        interval: 1000,
+      });
+    });
