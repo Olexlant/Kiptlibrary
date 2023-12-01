333129329f875831204bc3c4f0b2ee01a1fd18bf
Olexlant <lantuh.sasha@gmail.com>
2023-12-01 16:49:24 +0200

    fix

--- src/main/java/com/Kipfk/Library/bookFiles/BookFiles.java
+++ src/main/java/com/Kipfk/Library/bookFiles/BookFiles.java
@@ -1,6 +1,8 @@
 package com.Kipfk.Library.bookFiles;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,6 +11,8 @@ import javax.persistence.*;
 @Getter
 @Setter
 @Entity
+@EqualsAndHashCode
+@NoArgsConstructor
 public class BookFiles {
 
     @SequenceGenerator(
